e609efee105d496e18df687050f5085f4e40adeb
Subham <subhamy142@gmail.com>
2021-03-28 17:37:04 +0530

    added route and controller template

---
+++ controllers/auth.js
@@ -0,0 +1,17 @@
+exports.register =(req,res,next)=>{
+ res.send("Register Routes");
+};
+
+exports.login =(req,res,next)=>{
+    res.send("login Routes");
+};
+  
+  
+exports.forgotPassword =(req,res,next)=>{
+         res.send("forgotPassword Routes");
+};
+ 
+exports.resetPassword =(req,res,next)=>{
+    res.send("resetPassword Routes");
+};
+   
