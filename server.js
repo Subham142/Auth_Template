7afe6f4307a53d88642bebbc36434afada13744a
Subham <subhamy142@gmail.com>
2021-03-30 19:15:26 +0530

    connected mongoDB

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 require('dotenv').config({path:"./.env"});
 const express = require('express');
-
+const connetDB = require('./config/db');
 const app= express();
+const connectDB = require('./config/db')
+
+connetDB();
+
+app.use(express.json());
+
 const PORT= process.env.PORT|| 5000;
 
-app.listen(PORT,()=>console.log(`Server is up and running at port ${PORT}`));
+const server = app.listen(PORT, () =>
+  console.log(`Sever running on port ${PORT}`)
+);
+
+process.on("unhandledRejection", (err, promise) => {
+    console.log(`Logged Error: ${err.message}`);
+    server.close(() => process.exit(1));
+  });
