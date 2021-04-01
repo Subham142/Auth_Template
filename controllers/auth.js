1394e83228b4af2ea1c38160721831715bb522c9
Subham <subhamy142@gmail.com>
2021-04-01 16:49:51 +0530

    added register and login controllers

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,59 @@
-exports.register =(req,res,next)=>{
- res.send("Register Routes");
+const User = require("../models/User")
+exports.register = async (req,res,next)=>{
+ const {username,email,password} = req.body;
+
+ try {
+     const user = await User.create({
+         username,
+         email,
+         password,
+     });
+
+     res.status(201).json({
+         sucess:true,
+         user,
+     });
+ } catch (error) {
+    res.status(500).json({
+        sucess:false,
+        error: error.message,
+    });
+ }
+
 };
 
-exports.login =(req,res,next)=>{
-    res.send("login Routes");
+exports.login = async (req,res,next)=>{
+    const {email,password} = req.body;
+
+    if(!email || !password){
+        res.status(400).json({sucess:false,error:"Please provide email and password"})
+    }
+
+    try {
+        const user = await  User.findOne({email}).select("+password");
+
+        if(!user){
+            res.status(400).json({sucess:false,error:"Invalid credentials"})
+        }
+
+        const isMatch = await user.macthPassword(password);
+        
+        if(!isMatch){
+            res.status(400).json({sucess:false,error:"Invalid credentials"})
+        }
+
+        res.status(201).json({
+            sucess:true,
+            token:"asfsdfsd"
+        };)
+            
+
+    } catch (error) {
+        res.status(500).json({
+            sucess:false,
+            error:error.message
+        })
+    }
 };
   
   
